feccbc06dc4cb9922c43ebd77283b5d28201feef
James Carlson <carlso70@purdue.edu>
2016-09-15 23:51:39 -0400

    Routing added

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,17 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
